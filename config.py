f8397f5899746b0f713c0989b85d02e74586b596
yukka-pukka <76982244+yukka-pukka@users.noreply.github.com>
2026-04-04 11:28:42 -0700

    Update config.py
    on .gitignore, not sure why it still showed up

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import secrets
 
-# DATABASE_URL = "postgresql://tasty_user:password123@localhost:5432/tastybites"
+# DATABASE_URL = ""
 
 # Yelp API
-YELP_API_KEY = 'unksOTGmrK1Je7maJwHNmXehQnchaSCCZwKyn-lx_u5dkj_DVfKayIqL6vyWxCo7PD4oJZoJOyVfFHoFo9gPGm1axz9j2EuNyBZtfx338Q4eUHWsWHW6bjHyzgjQaXYx'
+YELP_API_KEY = ''
 
 # Auth / JWT
 SECRET_KEY = secrets.token_hex(32)
 ALGORITHM = "HS256"  
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  
 
-GOOGLE_MAPS_API_KEY = "AIzaSyB83H91nCB3m2OomaeejNhfWJBz6uGDBXI"
+GOOGLE_MAPS_API_KEY = ""
